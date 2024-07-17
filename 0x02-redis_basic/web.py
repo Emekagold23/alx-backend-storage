309efa6989a019e0c9554a3d788711c659003f00
Emekagold23 <onyekachigold23@gmail.com>
2024-07-17 12:32:31 +0000

    Redis basic

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,42 +1,36 @@
 #!/usr/bin/env python3
-'''Web caching and tracking.
-'''
-import redis
+"""
+web tools for caching and tracking
+"""
 import requests
+import redis
 from functools import wraps
-from typing import Callable
+
+store = redis.Redis()
 
 
-redis_store = redis.Redis()
-'''The module-level Redis instance.
-'''
-
-
-def data_cacher(method: Callable) -> Callable:
-    '''Caches the output of fetched data and tracks access counts.
-    '''
+def count_url_access(method):
+    """ Decorator counting how many times
+    a URL is accessed """
     @wraps(method)
-    def invoker(url) -> str:
-        '''The wrapper function for caching the output and tracking accesses.
-        '''
-        redis_store.incr(f'count:{url}')
-        result = redis_store.get(f'result:{url}')
-        if result:
-            return result.decode('utf-8')
-        result = method(url)
-        redis_store.setex(f'result:{url}', 10, result)
-        return result
-    return invoker
+    def wrapper(url):
+        cached_key = "cached:" + url
+        cached_data = store.get(cached_key)
+        if cached_data:
+            return cached_data.decode("utf-8")
+
+        count_key = "count:" + url
+        html = method(url)
+
+        store.incr(count_key)
+        store.set(cached_key, html)
+        store.expire(cached_key, 10)
+        return html
+    return wrapper
 
 
-@data_cacher
+@count_url_access
 def get_page(url: str) -> str:
-    '''Returns the content of a URL after caching the request's response,
-    and tracking the request.
-    '''
-    return requests.get(url).text
-
-# Example usage
-if __name__ == "__main__":
-    url = "http://slowwly.robertomurray.co.uk"
-    print(get_page(url))
+    """ Returns HTML content of a url """
+    res = requests.get(url)
+    return res.text
